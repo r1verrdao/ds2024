759a2ed934c4f0961325e8d18bb0939ec5a896c8
r1verrdao <riverdao36@gmail.com>
2024-04-03 15:10:37 +0700

    feat: add layer to model

--- onnx/VGG.py
+++ onnx/VGG.py
@@ -11,11 +11,28 @@ from sklearn.model_selection import train_test_split
 # load model VGG16
 based_model = vgg16.VGG16(weights = 'imagenet',
                     include_top = False,
-                    input_shape = (img_rows, img_cols, 3))
+                    input_shape = (224, 224, 3))
 
 # Freeze layers, not training these layers
 for layer in based_model.layers:
     layer.trainable = False 
 
 # Summary model 
+print("The base model: ")
 based_model.summary()
+
+def layer_added(output_based_network, num_classes):
+  x = output_based_network
+  x = layers.Flatten()(x)
+  x = layers.Dense(1024, activation='relu')(x)
+  x = layers.Dense(256, activation='relu')(x)
+  x = layers.Dense(num_classes, activation='softmax')(x)
+
+  return x
+
+num_classes = 19
+output_based_network = based_model.output 
+output_layer = layer_added(output_based_network, num_classes)
+model = Model(based_model.input, output_layer)
+print("The model after add output layers: \n")
+model.summary()
